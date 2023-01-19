cf731b9ce48ffddd87e6d8826c800989b2a6d54c
TaisukeMiyachi <taisuke.m.lotus.elise@gmail.com>
2023-01-19 23:40:07 +0900

    add data

--- portfolio_db.sql
+++ portfolio_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: localhost
--- 生成日時: 2023 年 1 月 12 日 15:49
+-- 生成日時: 2023 年 1 月 19 日 15:39
 -- サーバのバージョン： 10.4.27-MariaDB
 -- PHP のバージョン: 8.1.12
 
@@ -45,7 +45,10 @@ INSERT INTO `choice2023` (`id`, `name`, `number`, `first`, `second`, `third`, `c
 (3, '宮地泰介', 813, 2, 0, 5, '2023-01-11 23:50:55'),
 (4, '織田信長', 9, 0, 2, 4, '2023-01-12 23:44:43'),
 (5, '豊臣秀吉', 10, 2, 3, 5, '2023-01-12 23:46:29'),
-(7, '徳川家康', 11, 4, 5, 1, '2023-01-12 23:48:04');
+(7, '徳川家康', 11, 4, 5, 1, '2023-01-12 23:48:04'),
+(9, '宮地泰介', 8, 0, 1, 2, '2023-01-19 14:51:21'),
+(10, '宮地泰介', 8, 5, 2, 4, '2023-01-19 19:44:13'),
+(11, '宮地泰介', 8, 0, 0, 2, '2023-01-19 19:46:32');
 
 -- --------------------------------------------------------
 
@@ -57,18 +60,23 @@ CREATE TABLE `members` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(100) NOT NULL
+  `password` varchar(100) NOT NULL,
+  `first` int(100) DEFAULT NULL,
+  `second` int(100) DEFAULT NULL,
+  `third` int(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- テーブルのデータのダンプ `members`
 --
 
-INSERT INTO `members` (`id`, `name`, `email`, `password`) VALUES
-(8, '宮地泰介', 'test1@gs.com', '$2y$10$XiWquIX1jSwb.ZMfnBdsneQVAR2yaBEAsIOvT2LhGiW7nFsftEmRm'),
-(9, '織田信長', 'test2@gs.com', '$2y$10$JjKl0JoudNCGfnp4doCJI.VLEAId0PcNxHnlecEdfJVCkj/SV6doG'),
-(10, '豊臣秀吉', 'test3@gs.com', '$2y$10$oS4lP75IKOnQOShkIUYYB.dbzZwFyj/btjqVyz7vkMmsskegZAiWC'),
-(11, '徳川家康', 'test4@gs.com', '$2y$10$BtJW3NqGWCTEu9FjSCk2b.4aXp3KqWq0/Opjcj.GLUqqAFQIdkKly');
+INSERT INTO `members` (`id`, `name`, `email`, `password`, `first`, `second`, `third`) VALUES
+(8, '宮地泰介', 'test1@gs.com', '$2y$10$XiWquIX1jSwb.ZMfnBdsneQVAR2yaBEAsIOvT2LhGiW7nFsftEmRm', 0, 3, 1),
+(9, '織田信長', 'test2@gs.com', '$2y$10$JjKl0JoudNCGfnp4doCJI.VLEAId0PcNxHnlecEdfJVCkj/SV6doG', 0, 0, 0),
+(10, '豊臣秀吉', 'test3@gs.com', '$2y$10$oS4lP75IKOnQOShkIUYYB.dbzZwFyj/btjqVyz7vkMmsskegZAiWC', 0, 0, 0),
+(11, '徳川家康', 'test4@gs.com', '$2y$10$BtJW3NqGWCTEu9FjSCk2b.4aXp3KqWq0/Opjcj.GLUqqAFQIdkKly', 0, 0, 0),
+(12, '田中真', 'test1@google.com', '$2y$10$NqVVvARSD/iEUb4ZGjxF3uYzVJnnmMnvAsvzcCAtHZpKxpL03tb66', 0, 0, 0),
+(13, '鈴木一郎', 'test5@gs.com', '$2y$10$nvHDysBJsHcUFqN157qY7eT5IS4slVzEfdLaF04x6JmDH7grJFMZW', 2, 1, 4);
 
 --
 -- ダンプしたテーブルのインデックス
@@ -94,13 +102,13 @@ ALTER TABLE `members`
 -- テーブルの AUTO_INCREMENT `choice2023`
 --
 ALTER TABLE `choice2023`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- テーブルの AUTO_INCREMENT `members`
 --
 ALTER TABLE `members`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
